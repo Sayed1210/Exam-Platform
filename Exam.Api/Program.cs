b5a0ce0a0d81cb1e5f2434cf9a42ddc8aff39d04
ssamak <shaymaa.samak@gmail.com>
2026-04-28 13:49:42 +0300

    add candidate api

--- Exam.Api/Program.cs
+++ Exam.Api/Program.cs
@@ -1,12 +1,17 @@
 using Scalar.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using Exam.Data;
-
+using Exam.Repositories;
+using Exam.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// DI Candidate
+builder.Services.AddScoped<ICandidateRepository, CandidateRepository>();
+builder.Services.AddScoped<ICandidateService, CandidateService>();
 
 builder.Services.AddDbContext<ApiContext>(options =>
     options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection")!));
@@ -21,6 +26,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Candidate
+app.MapCandidateEndpoints();
 
 app.Run();
 
