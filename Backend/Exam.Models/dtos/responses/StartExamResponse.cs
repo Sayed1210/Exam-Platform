9d70b266d9a18fce674725c47eb631bb45e44ae8
Elsayed Abdelfattah <116787113+Sayed1210@users.noreply.github.com>
2026-05-25 13:31:24 +0300

    add view model for exam

--- Backend/Exam.Models/dtos/responses/StartExamResponse.cs
+++ Backend/Exam.Models/dtos/responses/StartExamResponse.cs
@@ -2,5 +2,20 @@ namespace Exam.Models;
 
 public class StartExamResponse
 {
-    public List<QuestionInExamResponse> Questions { get; set; } = [];
+    public List<CandidateQuestionInExamResponse> Questions { get; set; } = [];
+}
+
+public class CandidateQuestionInExamResponse
+{
+    public int Id { get; set; }
+    public string Text { get; set; } = string.Empty;
+    public string? ImageUrl { get; set; }
+    public List<CandidateChoiceInExamResponse> Choices { get; set; } = [];
+}
+
+public class CandidateChoiceInExamResponse
+{
+    public int Id { get; set; }
+    public string Text { get; set; } = string.Empty;
+    public string? ImageUrl { get; set; }
 }
